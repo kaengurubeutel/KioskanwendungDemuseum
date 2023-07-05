322d8e94821aae6d2ef7b788c38db85ceb327afd
Maximilian Uhlig <smuhlig@outlook.de>
2023-07-05 16:57:23 +0200

    init

--- web-src/src/App.vue
+++ web-src/src/App.vue
@@ -10,6 +10,16 @@ const click = () => {
   eel.hello_world()
   console.log("clicked")
 }
+
+let onBegin = () => {
+  console.log('=== Begin ===');
+}
+
+let onEnd = () => {
+  console.log('=== End ===');
+}
+
+
 </script>
 
 <template>
@@ -17,4 +27,25 @@ const click = () => {
     <p>{{ greeting }}</p>
     <button type="button" @click="click()">Click me</button>
   </div>
+  <div id="app">
+    <VueSignaturePad
+      width="500px"
+      height="500px"
+      ref="signaturePad"
+      :options="{ onBegin, onEnd }"
+    />
+  </div>
+
+  <div class="menubutton">
+    Löschen
+  </div>
+  <div class="menubutton" >
+    Zurück
+  </div>
+  <div class="menubutton" > 
+    Abschicken
+  </div>
+
+
+
 </template>
